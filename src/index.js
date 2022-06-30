33ca051fa7eb58462e7effdf038411362fa96d85
Victor Carli <victorcarlibr@gmail.com>
2022-06-30 00:05:26 -0300

    feat: create todos

--- src/index.js
+++ src/index.js
@@ -50,7 +50,20 @@ app.get('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body
+  const { username } = request
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  username.todos.push(todo)
+
+  return response.status(201).send()
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
