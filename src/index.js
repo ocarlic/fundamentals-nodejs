0b1e436ab30ed434f96d344671b9123f6cbc0c34
Victor Carli <victorcarlibr@gmail.com>
2022-06-29 23:28:41 -0300

    feat: created route users and middleware

--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.header
+
+  const user = users.find(user => user.username === username)
+
+  if(!user) {
+    return response.status(400).json({ error: 'User not found' })
+  }
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body
+
+  const userAlreadyExist = users.some(user => user.username === username)
+
+  if(userAlreadyExist) {
+    return response.status(400).json({ error: 'User already exists!' })
+  }
+
+  users.push({
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  })
+
+  return response.status(201).json(users)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
