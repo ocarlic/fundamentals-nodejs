b30b91ed2741a7c6cd59c780a73a1b2719f91f5f
Victor Carli <victorcarlibr@gmail.com>
2022-07-04 20:50:04 -0300

    feat: added delete in todo

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 
   user.todos.push(todo)
 
-  return response.status(201).send()
+  return response.status(201).send(todo)
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
@@ -75,18 +75,44 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 
   const todo = user.todos.find(todo => todo.id === id)
 
-  todo.title = title
-  todo.deadline = new Date(deadline)
+  if (todo) {
+    todo.title = title
+    todo.deadline = new Date(deadline)
+  
+    return response.status(201).send(todo)
+  }
 
-  return response.status(201).send(todo)
+  return response.status(404).json({ error: 'Todo not found' })
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params
+  const { user } = request
+
+  const todo = user.todos.find(todo => todo.id === id)
+  
+  if(todo) {
+    todo.done = true
+
+    return response.status(201).send(todo)
+  }
+
+  return response.status(404).json({ error: 'Todo not found' })
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params
+  const { user } = request
+
+  const todoIndex = user.todos.findIndex(todo => todo.id === id)
+
+  if (todoIndex === -1) {
+    return response.status(404).json({ error: 'Todo not found' }) 
+  }
+
+  user.todos.splice(todoIndex, 1)
+
+  return response.status(204).send()
 });
 
 module.exports = app;
